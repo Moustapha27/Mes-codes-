95cdffb1207f30eec2781a582cd16bb8fa75ba2b
Mouhamed SANKHANE <mouhamed-moustapha.sankhane@bedrockstreaming.com>
2024-11-12 22:19:13 +0100

    feat: Adding streamlit

--- Apprentissage_python/python_all_in_one.py
+++ Apprentissage_python/python_all_in_one.py
@@ -22,43 +22,46 @@
 # print(Z.real)
 # print(Z.imag)
 
+import streamlit as st
 import datetime as dt
 
+
 a = dt.date.today()
-print(a)
+st.write(f"Date d'aujourd'hui : {a}")
 
+# Fonction pour calculer l'âge
 def calculate_age(a):
-    A = input("Entrez votre année de naissance (format AAAA) : ")
-    if len(A) != 4:
-        print("Une année est composée de quatre chiffres")
-        return None
+    # Utilisation des champs Streamlit pour l'interaction
+    A = st.text_input("Entrez votre année de naissance (format AAAA) :")
+    M = st.text_input("Entrez votre mois de naissance (format mm) :")
+    J = st.text_input("Entrez votre jour de naissance (format jj) :")
+
+    # Vérifie que les entrées sont correctes
+    if A and M and J:
+        try:
+            A = int(A)
+            M = int(M)
+            J = int(J)
+            if len(str(A)) != 4 or not (1 <= M <= 12) or not (1 <= J <= 31):
+                st.write("Veuillez entrer une date valide.")
+                return None
+            
+            # Date de naissance
+            birth_date = dt.date(A, M, J)
+            age_years = a.year - birth_date.year
+            if (a.month, a.day) < (birth_date.month, birth_date.day):
+                age_years -= 1
+            
+            return age_years
+        except ValueError:
+            st.write("Veuillez entrer des valeurs numériques valides.")
+            return None
+    return None
+
+# Bouton pour lancer le calcul de l'âge
+if st.button("Calculer l'âge"):
+    W = calculate_age(a)
+    if W is not None:
+        st.write(f"Votre âge est : {W} ans")
     else:
-        A = int(A)
-
-    M = input("Entrez votre mois de naissance (format mm) : ")
-    if len(M) != 2:
-        print("Un mois est composé de deux chiffres")
-        return None
-    else:
-        M = int(M)
-
-    J = input("Entrez votre jour de naissance (format jj) : ")
-    if len(J) != 2:
-        print("Un jour est composé de deux chiffres")
-        return None
-    else:
-        J = int(J)
-
-    # Date de naissance
-    birth_date = dt.date(A, M, J)
-
-    age_years = a.year - birth_date.year
-    if (a.month, a.day) < (birth_date.month, birth_date.day):
-        age_years -= 1  
-
-    return age_years
-
-W = calculate_age(a)
-
-if W is not None:
-    print(f"Votre âge est : {W} ans")
+        st.write("Erreur dans le calcul de l'âge.")
